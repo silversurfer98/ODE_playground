35ce27fe95dca46de9b744fd4a6c7dbe1e6a9ff8
silversurfer98 <sraghul360@gmail.com>
2022-07-07 20:07:57 +0530

    added PID

--- tank.py
+++ tank.py
@@ -8,18 +8,43 @@ def tank(h,t,fi,p,g,r,a):
 
 
 
-fi = 1.1
-r = 0.09
+fi = 2
 a = 10.0
 p = 1.0
 g = 1.0
 
 #initial cond
-h0 = 0.5
+#h0 = 0.1
 
-t = np.linspace(0,30,100)
+t = np.linspace(0,120,400)
+h = np.zeros(len(t))
 
-h = odeint(tank,h0,t,args=(fi,p,g,r,a,))
+h0 = 0
+
+r = np.ones(len(t))*0.1
+
+sp = np.ones(len(t))*0.1
+sp[100:] = 1
+sp[300:] = 1.5
+error = np.zeros(len(t))
+
+kc = 2.1
+taui = 250.0
+taud = 5
+sum = 0
+lasterror = 0
+
+for i in range(len(t)-1):
+    error[i] = sp[i] - h[i]
+    sum = sum + (kc/taui)*error[i]
+    d = kc/taud * (error[i] - lasterror)
+    r[i] = r[0] + kc*error[i] + sum + d
+    temp = odeint(tank,h0,[t[i],t[i+1]],args=(fi,p,g,r[i],a,))
+    #print(temp)
+    h0 = temp[1]
+    h[i+1] = temp[1]
+    lasterror = error[i]
 
 plt.plot(t,h)
+plt.plot(t,sp,'r--')
 plt.show()
