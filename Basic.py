9cd5532adfc0af8da1f71c0b1be40fa0b10b0846
RAGHAVENDAR SRIDHAR <RAGHAVENDAR.SRIDHAR@saipem.com>
2022-08-09 11:52:27 +0530

    working rk4 method

--- Basic.py
+++ Basic.py
@@ -13,16 +13,17 @@ y0 = 0.1
 
 # define arguments
 a = 2
-b = 0
+b = 1.5
 # always group them in tuples
 arguments = (a,b,)
 
 #define time points  --> here 50 means 50 pts inbetween 0 and 20
-t = np.linspace(0,10,1000)
+t = np.linspace(0,5,100)
 
 # now we ready to solve
 y = odeint(model,y0,t,args=arguments)
-
+print(t)
+print(y)
 #now graph it
 plt.plot(t,y)
 #plt.yscale('log')
--- c++/ode_rk4.cpp
+++ c++/ode_rk4.cpp
@@ -1,17 +1,17 @@
 #include"ode_rk4.h"
 #include<iostream>
 
-const float h = 0.00001;
+const float h = 0.000001;
 float k1,k2,k3,k4;
 
-float model(/*parameters*/float y, float t)
+float model(/*parameters*/float y, float t, float* args)
 {
     float dydt;
-    dydt = y*2;
+    dydt = y* args[0] + args[1];
     return dydt;
 }
 
-float rk4(float f, float start_time, float end_time)
+float rk4(float f, float start_time, float end_time, float *args)
 {
     k1 = 0;
     k2 = 0;
@@ -20,12 +20,12 @@ float rk4(float f, float start_time, float end_time)
     // std::cout<<"start time --> "<<start_time<<"\n";
     while(start_time<end_time)
     {
-        k1 = h * model(f,start_time);
+        k1 = h * model(f,start_time, args);
         start_time = start_time + h*0.5;
-        k2 = h * model(f + k1*0.5, start_time);
-        k3 = h * model(f + k2*0.5, start_time);
+        k2 = h * model(f + k1*0.5, start_time, args);
+        k3 = h * model(f + k2*0.5, start_time, args);
         start_time = start_time + h*0.5;
-        k4 = h * model(f + k3, start_time);
+        k4 = h * model(f + k3, start_time, args);
         f = f + (k1/6) + (k2/3) + (k3/3) + (k4/6);
         //std::cout<<"time --> "<<start_time<<" k1 / k2 / k3 / k4 --> "<<k1<<" / "<<k2<<" / "<<k3<<" / "<<k4<<"\n";
     }
@@ -33,19 +33,58 @@ float rk4(float f, float start_time, float end_time)
     return f;
 }
 
+void allocate_mem(float** y, float** t, float start_time, float end_time, unsigned int size)
+{
+    float *tf = new float[size];
+    tf[0] = start_time;
+
+    float *yf = new float[size];
+    yf[0] = 0.0;
+
+    float step = (end_time - start_time) / (size - 1);
+
+    for(/*unsigned */int i=1;i<size;i++)
+    {
+        start_time = start_time + step;
+        // std::cout<<i<<" test "<<start_time<<"\n";
+        tf[i] = start_time;
+        yf[i] = 0.0;
+    }
+    *t = tf;
+    *y = yf;
+
+}
+
 int main ()
 {
-    float* t = new float[10];
-    for(unsigned int i=0;i<10;i++)
-        t[i] = (float)i;
+    // define arguments to the model
+    float *args = new float[2];
+    args[0] = 2.0;
+    args[1] = 1.5;
 
-    float* y = new float[10];
+    // define start_time, end_time, and resolution sample points for memory allocation
+    float start_time = 0.0;
+    float end_time = 5.0;
+    unsigned int resolution = 100;
+
+    float *t, *y;
+    allocate_mem(&y,&t,start_time,end_time,resolution);
+
+    // for(unsigned int i=0;i<resolution;i++)
+    // {
+    //     std::cout<<t[i]<<"\n";
+    // }
+
     y[0] = 0.1;
-    for(unsigned int i=1;i<10;i++)
+    for(unsigned int i=1;i<resolution;i++)
     {
-        y[i] = rk4(y[i-1],t[i-1],t[i]);
-        std::cout<<"the value at time "<<t[i]<<"th sec is --> "<<y[i]<<"\n";
+        y[i] = rk4(y[i-1],t[i-1],t[i], args);
+        //std::cout<<"the value at time "<<t[i]<<"th sec is --> "<<y[i]<<"\n";
+        std::cout<<y[i]<<"\n";
     }
+
+    delete[] y;
+    delete[] t;
 
     return 0;
 }
